fafcfb4a0a7b07d77199a294fc6ae813fdb79a37
lakestorey <lakebeforestorey@gmail.com>
2016-09-29 12:37:48 -0400

    finished product

--- GreetingCard/Form1.Designer.cs
+++ GreetingCard/Form1.Designer.cs
@@ -29,7 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(greetingCard));
+            this.cruxLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // cruxLabel
+            // 
+            this.cruxLabel.AutoSize = true;
+            this.cruxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cruxLabel.Location = new System.Drawing.Point(12, 18);
+            this.cruxLabel.Name = "cruxLabel";
+            this.cruxLabel.Size = new System.Drawing.Size(190, 26);
+            this.cruxLabel.TabIndex = 0;
+            this.cruxLabel.Text = "Crux Constellation";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(332, 205);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(97, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "By: Lake Storey";
             // 
             // greetingCard
             // 
@@ -37,15 +59,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.ClientSize = new System.Drawing.Size(441, 227);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.cruxLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "greetingCard";
             this.Text = "Greeting Card";
             this.Click += new System.EventHandler(this.greetingCard_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label cruxLabel;
+        private System.Windows.Forms.Label nameLabel;
     }
 }
 
--- GreetingCard/Form1.cs
+++ GreetingCard/Form1.cs
@@ -26,6 +26,11 @@ namespace GreetingCard
 
         private void greetingCard_Click(object sender, EventArgs e)
         {
+            //makes labels invisible
+            nameLabel.Visible = false;
+            cruxLabel.Visible = false;
+
+            //creates all graphics brushes and fonts
             Graphics formGraphics = this.CreateGraphics();
             Font tnr16 = new Font("Times New Roman", 16, FontStyle.Bold);
             Font tnr18 = new Font("Times New Roman", 18, FontStyle.Bold);
@@ -33,15 +38,16 @@ namespace GreetingCard
             SolidBrush whiteBrush = new SolidBrush(Color.White);
             SolidBrush blueBrush = new SolidBrush(Color.Blue);
             SolidBrush redBrush = new SolidBrush(Color.Red);
-            SolidBrush greenBrush = new SolidBrush(Color.Green);
             Pen yellowPen = new Pen(Color.Yellow, 4);
 
+            //declares all sounds
             SoundPlayer pewPew = new SoundPlayer(Properties.Resources.Pew_Pew_DKnight556_1379997159);
-            SoundPlayer pingSound = new SoundPlayer(Properties.Resources.glass_ping_Go445_1207030150);
             SoundPlayer drummingSound = new SoundPlayer(Properties.Resources.Civil_War_Drummer__SoundBible_com_700036269);
 
+            //sets the graphics object to black
             formGraphics.Clear(Color.Black);
 
+            //creates crux constellation
             Thread.Sleep(100);
             formGraphics.DrawLine(yellowPen, 106, 75, 38, 104);
             formGraphics.FillEllipse(whiteBrush, 38, 98, 10, 10);
@@ -53,21 +59,25 @@ namespace GreetingCard
             formGraphics.FillEllipse(whiteBrush, 99, 150, 10, 10);
             pewPew.Play();
             Thread.Sleep(100);
+
+            //creates informational paragraph about constellation
             formGraphics.DrawString("The Crux", tnr16, redBrush, 90, 40);
-            formGraphics.DrawString("The Crux", tnr16, greenBrush, 90, 40);
+            Thread.Sleep(200);
             formGraphics.DrawString("The crux is a constellation located in the", castellar7, blueBrush, 130, 70);
+            Thread.Sleep(200);
             formGraphics.DrawString("southern sky in a bright portion of the milky", castellar7, blueBrush, 130, 85);
+            Thread.Sleep(200);
             formGraphics.DrawString("way. The Crux is bordered by Centaurus on", castellar7, blueBrush, 130, 100);
+            Thread.Sleep(200);
             formGraphics.DrawString("the eastern, northern and western front and", castellar7, blueBrush, 130, 115);
+            Thread.Sleep(200);
             formGraphics.DrawString("by Musca to the south. Crux is easily visible", castellar7, blueBrush, 130, 130);
+            Thread.Sleep(200);
             formGraphics.DrawString("From the south hemisphere.", castellar7, blueBrush, 130, 145);
             Thread.Sleep(100);
 
-            for (int i = 0; i <=10; i++)
-            {
-                drummingSound.Play();
-                Thread.Sleep(1000);
-            }
+            //plays drumming sound
+            drummingSound.Play();
         }
     }
 }
